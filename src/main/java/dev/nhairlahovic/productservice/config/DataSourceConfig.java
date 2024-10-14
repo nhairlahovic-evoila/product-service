6efa003aee6742c54980797d4d5372bccb1cd326
Nedim Hairlahovic <nhairlahovic@evoila.com>
2024-10-14 23:12:55 +0200

    Add Cloud Foundry service database config

---
+++ src/main/java/dev/nhairlahovic/productservice/config/DataSourceConfig.java
@@ -0,0 +1,72 @@
+package dev.nhairlahovic.productservice.config;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        String vcapServices = System.getenv("VCAP_SERVICES");
+
+        if (vcapServices == null || vcapServices.isEmpty()) {
+            // Fallback to H2 in-memory database if VCAP_SERVICES is not found
+            log.info("VCAP_SERVICES not found");
+            throw new RuntimeException("VCAP_SERVICES not found");
+        }
+
+        try {
+            log.info("VCAP_SERVICES found: {}", vcapServices);
+
+            // Parse the VCAP_SERVICES environment variable to extract the credentials
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.readTree(vcapServices);
+            JsonNode credentials = root.path("osb-postgresql-test").get(0).path("credentials");
+
+            String url = credentials.get("uri").asText();
+            String username = credentials.get("user").asText();
+            String password = credentials.get("password").asText();
+
+            String jdbcUrl = getJdbcUrl(url);
+
+            // Return a DataSource configured with the extracted credentials
+            return postgresDataSource(jdbcUrl, username, password);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse VCAP_SERVICES", e);
+        }
+    }
+
+    private String getJdbcUrl(String url) {
+        String uri = url.substring("postgres://".length());
+        uri = uri.replace("sslmode=verify-full", "sslmode=disable");
+
+        // Extract user and password
+        String[] userPassAndHost = uri.split("@");
+
+        // Extract host part and construct JDBC URL including the query parameters
+        String[] hostAndParams = userPassAndHost[1].split("\\?", 2);
+        String hostAndPath = hostAndParams[0];
+        String queryParams = hostAndParams.length > 1 ? "?" + hostAndParams[1] : "";
+
+        return "jdbc:postgresql://" + hostAndPath + queryParams;
+    }
+
+    private DataSource postgresDataSource(String url, String username, String password) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+}
