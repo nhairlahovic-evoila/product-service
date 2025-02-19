f6484a9bb88f5dee091b5f2aa3ae7d59aefd45af
Nedim Hairlahovic <nhairlahovic@evoila.com>
2025-02-19 16:45:26 +0100

    Add validation and error handling

--- src/main/java/dev/nhairlahovic/productservice/model/Product.java
+++ src/main/java/dev/nhairlahovic/productservice/model/Product.java
@@ -1,10 +1,14 @@
 package dev.nhairlahovic.productservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "products")
 public class Product {
 
